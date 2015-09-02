5338bff3ef6ba51535c1b1a16e4e33f6cf7d0a90
Élio Severiano <eliofilipe@gmail.com>
2015-09-02 15:47:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -76,8 +76,6 @@ xterm*|rxvt*)
 esac
 
 # enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
@@ -85,7 +83,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
-fi
 
 # some more ls aliases
 alias ll='ls -alF'
@@ -112,9 +109,6 @@ alias mt="tail -f /var/log/mail.log"
 alias st="tail -f /var/log/syslog"
 
 EOF
-
-bash 
-
 
 # Check if user is root
 if [ $(id -u) != "0" ]; then
